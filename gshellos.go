f44cf8db9d707846deeb4dc6767cd369d3317deb
Bai Yingjie <byj.tea@gmail.com>
2021-05-27 17:46:44 +0000

    plan gre pull and gre push commands

--- gshellos.go
+++ gshellos.go
@@ -4,8 +4,10 @@ import (
 	"encoding/gob"
 	"errors"
 	"fmt"
+	"io"
 	"path"
 	"runtime"
+	"time"
 
 	"github.com/d5/tengo/v2"
 )
@@ -144,4 +146,56 @@ func ExtendObj(o tengo.Object) (tengo.Object, error) {
 	default:
 		return nil, ErrNotExtendable
 	}
+}
+
+type endlessReader struct {
+	r io.Reader
+}
+
+func (er endlessReader) Read(p []byte) (n int, err error) {
+	for i := 0; i < 30; i++ {
+		n, err = er.r.Read(p)
+		if err != io.EOF {
+			return
+		}
+		time.Sleep(100 * time.Millisecond)
+	}
+	p[n] = 0 // fake read
+	return n + 1, nil
+}
+
+type writerStat struct {
+	w   io.Writer
+	eof bool
+}
+type mWriter struct {
+	writers []*writerStat
+}
+
+func (mw *mWriter) Write(p []byte) (n int, err error) {
+	eofNum := 0
+	for _, w := range mw.writers {
+		if w.eof {
+			eofNum++
+			continue
+		}
+		_, terr := w.w.Write(p)
+		if terr != nil {
+			w.eof = true
+			eofNum++
+		}
+	}
+	if eofNum == len(mw.writers) {
+		return 0, io.EOF
+	}
+	return len(p), nil
+}
+
+// multiWriter is like io.MultiWriter but only stops if all writers are EOF.
+func multiWriter(writers ...io.Writer) io.Writer {
+	mw := &mWriter{}
+	for _, w := range writers {
+		mw.writers = append(mw.writers, &writerStat{w: w})
+	}
+	return mw
 }
