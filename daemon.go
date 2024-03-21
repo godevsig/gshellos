b27d32612b462dd58e79085b8f8a4b21657ed16d
Bai Yingjie <byj.tea@gmail.com>
2024-03-21 13:53:11 +0000

    correct help message

--- daemon.go
+++ daemon.go
@@ -432,6 +432,7 @@ type cmdInfo struct{}
 
 func (msg cmdInfo) Handle(stream as.ContextStream) (reply interface{}) {
 	var b strings.Builder
+	fmt.Fprintf(&b, "Arch: %s\n", runtime.GOARCH)
 	fmt.Fprintf(&b, "Version: %s\n", version)
 	fmt.Fprintf(&b, "Commit: %s\n", commitRev)
 	fmt.Fprintf(&b, "Build time: %s\n", buildTime)
